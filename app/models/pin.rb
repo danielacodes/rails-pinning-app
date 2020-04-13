9d6ec8d94637c85789733da1cad83f76ddd315c9
Daniela Vitosova <danielavitosova@seznam.cz>
2020-04-13 18:33:42 +0200

    Adding validation to upload correct image files

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,15 @@ class Pin < ActiveRecord::Base
   validates_uniqueness_of :slug
   belongs_to :category
   has_one_attached :image
+  validate :correct_image_type
+
+  private 
+  def correct_image_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:image, 'must be a JPEG or PNG')
+    elsif image.attached? == false
+      errors.add(:image, 'is required')
+    end
+  end
+
 end
