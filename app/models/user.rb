bca31dce90fd215dc0b8f0ea2b479bcc640c76ac
Daniela Vitosova <danielavitosova@seznam.cz>
2020-04-18 13:17:09 +0200

    Refactoring authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,10 @@ class User < ApplicationRecord
 	validates_uniqueness_of :email
   
   def self.authenticate(email, password)
-		@user = User.find_by_email(email)
-    if @user.present?
-      @user = User.find_by_email_and_password(email, password)
+    @user = User.find_by_email_and_password(email, password)
 			if @user.present?
         return @user
       end
-		end
 		return nil
 	end
 
